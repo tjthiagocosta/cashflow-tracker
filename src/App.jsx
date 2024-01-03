e22b532c0511ecda4da0f0bafef1b456c13faa79
Thiago Costa <86612543+tjthiagocosta@users.noreply.github.com>
2024-01-03 14:21:02 -0300

    Add app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import "./App.css";
 
 function App() {
-  return <div>Hello world!</div>;
+  return (
+    <main>
+      <div>
+        <h1>0</h1>
+        <div className="input-container">
+          <input type="text" placeholder="Income or Expense" />
+          <input type="number" />
+          <select>
+            <option value="income">Income</option>
+            <option value="expense">Expense</option>
+          </select>
+          <button>+</button>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
