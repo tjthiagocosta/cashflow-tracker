28988471e51c5f54b7f42671997bfff077a2499b
Thiago Costa <86612543+tjthiagocosta@users.noreply.github.com>
2024-01-04 15:06:40 -0300

    Add two way binding on input fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [statement, setStatement] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [statementType, setStatementType] = useState("income");
   return (
     <main>
       <div>
         <h1>0</h1>
         <div className="input-container">
-          <input type="text" placeholder="Income or Expense" />
-          <input type="number" />
-          <select>
+          <input
+            type="text"
+            placeholder="Income or Expense"
+            onChange={(e) => {
+              setStatement(e.target.value);
+            }}
+            value={statement}
+          />
+          <input
+            type="number"
+            placeholder="$"
+            onChange={(e) => {
+              setAmount(e.target.value);
+            }}
+            value={!!amount && ""}
+          />
+          <select
+            onChange={() => {
+              setStatementType(e.target.value);
+            }}
+            value={statementType}
+          >
             <option value="income">Income</option>
             <option value="expense">Expense</option>
           </select>
